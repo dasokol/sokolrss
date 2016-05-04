8246034b5983ebdafb8f29e3013224c5b464c26f
dasokol <dasokol@umich.edu>
2016-05-04 17:14:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def loadPage():
 	    <!-- jQuery library -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	    <!-- Latest compiled JavaScript -->
-	    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         </head>
         <body>
             <h1>Hello, David</h1>
