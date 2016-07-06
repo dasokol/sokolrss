d981d91d63fb8f0338ef949343b3b32a734e1eb0
dasokol <dasokol@umich.edu>
2016-07-06 07:29:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ def loadPage():
 	description = ""
 	
 	try:
-	    weather = getWeather()
-    	title = weather[0]
-	    description = weather[1]
+		weather = getWeather()
+    		title = weather[0]
+		description = weather[1]
 		sports = getSports()
     #date = getSports[0]
 	except:
-    	news = getNews()
+    		news = getNews()
 	
-    	history = getHistory()
+    		history = getHistory()
 	
 	
-    	'''
-    	f = open('index.html', 'r')
-    	return f.read()
-    	'''
-    	html = '''
-    	<!DOCTYPE html>
-    	<html>
+    		'''
+    		f = open('index.html', 'r')
+    		return f.read()
+    		'''
+    		html = '''
+    		<!DOCTYPE html>
+    		<html>
         	<head lang="en">
             	<meta charset="UTF-8">
 	    	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -76,10 +76,10 @@ def loadPage():
             	<h3>On this day in history:</h3>
             	<a href="%s">%s</p><br>
         	</body>
-    	</html>
-    	''' %(title, description[:-253], sports[0], sports[1], sports[2], sports[3], sports[4], sports[5], sports[6], sports[7],
+    		</html>
+    		''' %(title, description[:-253], sports[0], sports[1], sports[2], sports[3], sports[4], sports[5], sports[6], sports[7],
           	news[0], news[1], news[2], news[3], news[4], news[5], news[6], news[7], news[8], news[9], news[10], news[11],
           	news[12], news[13], news[14], history[0], history[1])
-    	return html
+    		return html
 	
 	
